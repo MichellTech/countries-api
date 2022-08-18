3405913e270c8fe1e8062dd5c6f966f47face3cb
Okwu chiedozie Michell <okwuchiedozie@gmail.com>
2022-08-18 18:50:19 +0100

    complete project

---
+++ src/home.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const url = 'https://restcountries.com/v3.1/all'
+
+function App() {
+  const [info, setInfo] = useState([])
+  const [query, setQuery] = useState('')
+  const [theme, setTheme] = useState(true)
+  const [loading, setLoading] = useState(true)
+  const [filterparams, setFilterparams] = useState('')
+  const [allData, setAllData] = useState([])
+  const [filterCont, setFilteredCont] = useState([])
+
+  const changeColor = () => {
+    setTheme(!theme)
+  }
+  const fetchCountry = async () => {
+    try {
+      const resposne = await fetch(url)
+      const data = await resposne.json()
+      setInfo(data)
+      setAllData(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchCountry()
+  }, [])
+
+  const fetchSearch = (value, arrayDatas) => {
+    let newCountry = arrayDatas.filter((item) =>
+      item.name.common.toLowerCase().includes(value.toLowerCase())
+    )
+    setInfo(newCountry)
+  }
+  useEffect(() => {
+    if (query && filterparams) {
+      return fetchSearch(query, filterCont)
+    }
+    if (query) {
+      fetchSearch(query, allData)
+    }
+  }, [query, filterparams])
+
+  const fetchCont = (value) => {
+    let newCont = allData.filter(
+      (item) => item.region.toLowerCase() === value.toLowerCase()
+    )
+    setInfo(newCont)
+    setFilteredCont(newCont)
+  }
+  useEffect(() => {
+    if (filterparams) {
+      fetchCont(filterparams)
+    } else {
+      setFilteredCont([])
+    }
+  }, [filterparams])
+
+  return (
+    <main className={`${theme ? 'white-theme' : 'dark-theme'} `}>
+      <header>
+        <nav className='section1'>
+          <div className='nav-bar'>
+            <h3>where in the world?</h3>
+            <button className='btn' onClick={changeColor}>
+              {theme ? 'Dark mood' : 'Light mood'}
+            </button>
+          </div>
+        </nav>
+      </header>
+      <section className='search section'>
+        <div className='search-form'>
+          <form>
+            <input
+              type='text'
+              placeholder='Search for a country'
+              className='input-search'
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </form>
+          <div>
+            <select
+              type='text'
+              placeholder='Filter Countries By Region'
+              value={filterparams}
+              onChange={(e) => setFilterparams(e.target.value)}
+              className='dropdown'
+            >
+              <option value='all'>Filter by continent</option>
+              <option value='Africa'>Africa</option>
+              <option value='Americas'>America</option>
+              <option value='Asia'>Asia</option>
+              <option value='Europe'>Europe</option>
+              <option value='Oceania'>Oceania</option>
+            </select>
+          </div>
+        </div>
+      </section>
+
+      <section className='section'>
+        <div className={`${loading ? 'loader' : ''} `}></div>
+        <div className='cards'>
+          {info.length > 0 ? (
+            info
+              .sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
+              .map((item, i) => {
+                const { name, capital, region, population, flags, idd } = item
+                return (
+                  <Link to={`/country/${name.common}`} key={name.common}>
+                    <div className='card' key={i}>
+                      <div className='card-image'>
+                        <img src={flags.png} alt='flag' className='flag' />
+                      </div>
+                      <div className='card-info'>
+                        <h2>{name.common}</h2>
+                        <p>
+                          Popultion:
+                          <span className='text-span'>{population}</span>
+                        </p>
+                        <p>
+                          region:<span className='text-span'>{region}</span>
+                        </p>
+                        <p>
+                          capital:<span className='text-span'>{capital}</span>
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                )
+              })
+          ) : (
+            <h1 className={`${loading ? 'error hide' : 'error'} `}>
+              No country found
+            </h1>
+          )}
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/singlecountry.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import flag from './images/flag.jpg'
+const endpoint = 'https://restcountries.com/v3.1/name/'
+
+const singlecountry = (i) => {
+  const [loadData, setLoadData] = useState([])
+  const { name } = useParams()
+
+  const hello = async () => {
+    let url
+    url = `${endpoint}${name}`
+    console.log(url)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setLoadData(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    hello()
+  }, [name])
+  // const {
+  //   population,
+  //   flags,
+  //   region,
+  //   subregion,
+  //   capital,
+  //   currencies,
+  //   languages,
+  //   borders,
+  // } = loadDatas
+  // console.log(population)
+  return (
+    <div className='main-div section'>
+      {loadData.map((item) => {
+        const {
+          name,
+          population,
+          flags,
+          region,
+          subregion,
+          capital,
+          currencies,
+          languages,
+          borders,
+        } = item
+        console.log(item)
+        return (
+          <div className='display-content'>
+            <div className='display-image'>
+              <img src={flags.png} alt='' className='flagging' />
+            </div>
+            <div className='display-info'>
+              <h2>{name.common}</h2>
+              <p>
+                Official name:
+                <span className='text-span'>{name.official}</span>
+              </p>
+              <p>
+                Population:
+                <span className='text-span'>{population}</span>
+              </p>
+
+              <p>
+                Region:
+                <span className='text-span'>{region}</span>
+              </p>
+              <p>
+                Sub Region:
+                <span className='text-span'>{subregion}</span>
+              </p>
+              <p>
+                Capital:
+                <span className='text-span'>{capital}</span>
+              </p>
+
+              {/* <p>
+                Languages:
+                <span className='text-span'>{languages}</span>
+              </p> */}
+              <p>
+                Border Countries:
+                <span className='text-span'>{borders[1]}</span>
+              </p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+export default singlecountry
